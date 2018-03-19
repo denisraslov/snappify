df0ec15273a267c9c385f1a4f608928c45e02200
Denis Raslov <denis.raslov@yandex.ru>
2018-03-19 12:10:06 +0300

    Add tests for boolean value generation

--- __tests__/valuesGenerator.js
+++ __tests__/valuesGenerator.js
@@ -1,9 +1,25 @@
 import generateValue from './../valuesGenerator';
 
 test('String generation', () => {
-    expect(generateValue('string', 'field')).toBe('\'field value\'');
-    expect(generateValue('string')).toBe('\'value\'');
-    expect(generateValue('string[]')).toBe('[\'value\', \'value\']');
-    expect(generateValue('Array<string>')).toBe('[\'value\', \'value\']');
-    expect(generateValue('()=>string')).toBe('() => \'value\'');
+  expect(generateValue('string', 'field')).toBe('\'field value\'');
+  expect(generateValue('string')).toBe('\'value\'');
+  expect(generateValue('string[]')).toBe('[\'value\', \'value\']');
+  expect(generateValue('Array<string>')).toBe('[\'value\', \'value\']');
+  expect(generateValue('()=>string')).toBe('() => \'value\'');
+});
+
+test('Boolean generation', () => {
+  const boolValues = [false, true];
+
+  expect(boolValues).toContain(generateValue('boolean', 'field'));
+  expect(boolValues).toContain(generateValue('boolean'));
+
+  const arrayOfBool1 = JSON.parse(generateValue('boolean[]'));
+  arrayOfBool1.forEach(value => expect(boolValues).toContain(value));
+
+  const arrayOfBool2 = JSON.parse(generateValue('Array<boolean>'));
+  arrayOfBool2.forEach(value => expect(boolValues).toContain(value));
+
+  const func = generateValue('()=>boolean');
+  expect(boolValues).toContain(eval(`(${func})()`));
 });
