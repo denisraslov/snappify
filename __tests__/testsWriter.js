67a8cd97175098c53d7d255627b25f029b453c58
Denis Raslov <denis.raslov@yandex.ru>
2018-03-20 13:50:00 +0300

    Add test for test generation

---
+++ __tests__/testsWriter.js
@@ -0,0 +1,22 @@
+const generateTest = require('./../writer').generateTest;
+
+const expectedTestContent =
+`test(\'ComponentName TestID\', () => {
+  const tree = renderer.create(
+    <ComponentName
+      id=1
+    />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+`;
+
+test('Test generation', async () => {
+  const testContent = await generateTest(
+    'ComponentName', 'TestID', 'id=1'
+  );
+
+   expect(testContent).toEqual(expectedTestContent);
+});
